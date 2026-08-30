443bf9ba21d9c4784b9f33eea39404e777962621
Wolf <71544385+if-shouldrs@users.noreply.github.com>
2026-08-30 15:26:16 +0100

    Added pause back to start scripts

--- scripts/release/start.bat
+++ scripts/release/start.bat
@@ -2,3 +2,4 @@
 call "%~dp0scripts\jdk\download.bat"
 if errorlevel 1 exit /b %ERRORLEVEL%
 "%~dp0jdk\bin\java.exe" -jar "%~dp0syncnuke-desktop.jar" %*
+pause
--- scripts/release/start.sh
+++ scripts/release/start.sh
@@ -5,3 +5,5 @@ set -e
 script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 "$script_dir/scripts/jdk/download.sh"
 exec "$script_dir/jdk/bin/java" -jar "$script_dir/syncnuke-desktop.jar" "$@"
+
+read -p "Press enter to continue..."
